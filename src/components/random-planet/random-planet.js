7d530abd29b9700d5613636f6603d70c0a8220f1
lizzochek <littled685@gmail.com>
2021-10-17 16:57:01 +0300

    Getting random id

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -21,7 +21,7 @@ export default class RandomPlanet extends Component {
   };
 
   updatePlanet() {
-    const id = 12;
+    const id = Math.floor(Math.random() * 25) + 2;
     this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
 
